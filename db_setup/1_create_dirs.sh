0ff50ce292ac580eb90ad4e18f14ceea2b4ceb19
Alessandro Modolo <alessandro.modolo@overit.it>
2022-09-16 22:53:31 +0200

    changeSet update to head of version 10.0 project structure improvement

--- db_setup/1_create_dirs.sh
+++ db_setup/1_create_dirs.sh
@@ -1,4 +1,5 @@
 echo Creating the tablespaced
 mkdir -p /var/lib/postgresql/data/pg_tablespaces/GEODATA
 mkdir -p /var/lib/postgresql/data/pg_tablespaces/GEOINDX
+mkdir -p /var/lib/postgresql/data/pg_tablespaces/GEOLOB
 echo Done
--- docker-entrypoint-initdb.d/2_create_tablespaces.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLESPACE GEODATA OWNER postgres LOCATION '/var/lib/postgresql/data/pg_tablespaces/GEODATA';
-
-CREATE TABLESPACE GEOINDX OWNER postgres LOCATION '/var/lib/postgresql/data/pg_tablespaces/GEOINDX';
