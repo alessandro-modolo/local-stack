c793be6c575a9f1a7375e1c56f6331350a817b13
Alessandro Modolo <alessandro.modolo@overit.it>
2022-09-18 22:45:49 +0200

    generate db changelog for the version 10.0

--- db_setup/1_create_dirs.sh
+++ db_setup/1_create_dirs.sh
@@ -1,4 +1,7 @@
 echo Creating the tablespaced
 mkdir -p /var/lib/postgresql/data/pg_tablespaces/USERS
 mkdir -p /var/lib/postgresql/data/pg_tablespaces/INDX
+mkdir -p /var/lib/postgresql/data/pg_tablespaces/GEODATA
+mkdir -p /var/lib/postgresql/data/pg_tablespaces/GEOINDX
+mkdir -p /var/lib/postgresql/data/pg_tablespaces/GEOLOB
 echo Done
